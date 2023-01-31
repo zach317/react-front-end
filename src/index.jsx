79e8fe969940fd0f75dd28fa997fd10dec154866
zach <zach000317@gmail.com>
2023-01-31 17:04:32 +0800

    feat: antd引入与配置

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.less'
 import App from './app.jsx'
+import zhCN from 'antd/es/locale/zh_CN'
+import { ConfigProvider } from 'antd'
+import 'moment/locale/zh-cn'
+import 'dayjs/locale/zh-cn';
+import dayjs from 'dayjs'
+import './index.less'
 
+dayjs.locale('zh-cn');
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  <ConfigProvider locale={zhCN}>
     <App />
-  </React.StrictMode>
+  </ConfigProvider>
 )
