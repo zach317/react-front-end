cd6a549847878e0b8cf8564dbc21fb5be88d27da
zach <zach000317@gmail.com>
2023-02-01 10:19:42 +0800

    feat: axios&proxy

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -8,5 +8,13 @@ module.exports = {
     open: true,
     //react配置路由之后，直接输入url显示404,是因为少了下面的配置
     historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000/',
+        pathRewrite: { '^/api': '' },
+        // changeOrigin: true,     // target是域名的话，需要这个参数，
+        // secure: false,          // 设置支持https协议的代理
+      },
+    },
   },
 }
