4bdb7d079588418ef2e7a07289509f00ff144d08
zach <zach000317@gmail.com>
2023-02-02 14:06:57 +0800

    feat: publicPath

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,11 +1,15 @@
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
+  output: {
+    publicPath: '/',
+  },
   devServer: {
     host: '127.0.0.1',
     hot: true,
     port: 9000,
     open: true,
+    // inline: true,
     //react配置路由之后，直接输入url显示404,是因为少了下面的配置
     historyApiFallback: true,
     proxy: {
