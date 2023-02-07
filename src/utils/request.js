fd69efe82d79a63b90ba4cb1ac028905439c674e
zach <zach000317@gmail.com>
2023-02-07 17:09:49 +0800

    feat: 封装axios拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,30 +1,33 @@
 import axios from 'axios'
 
-export const request = axios.create({
+const request = axios.create({
   timeout: 5000,
   baseURL: '/api',
 })
 
 // 添加请求拦截器
 request.interceptors.request.use(
-  function (config) {
-    // 在发送请求之前做些什么
+  // 在发送请求之前做些什么
+  (config) => {
+    const token = localStorage.getItem('token')
+    // eslint-disable-next-line no-param-reassign
+    config.headers.Authorization = `Bearer ${token}`
     return config
   },
-  function (error) {
-    // 对请求错误做些什么
-    return Promise.reject(error)
-  }
+  // 对请求错误做些什么
+  (error) => Promise.reject(error)
 )
 
 // 添加响应拦截器
 request.interceptors.response.use(
-  function (response) {
-    // 对响应数据做点什么
+  // 对响应数据做点什么
+  (response) => {
+    const { authorization } = response.headers
+    authorization && localStorage.setItem('token', authorization)
     return response.data
   },
-  function (error) {
-    // 对响应错误做点什么
-    return Promise.reject(error.response.data)
-  }
+  // 对响应错误做点什么
+  (error) => Promise.reject(error.response.data)
 )
+
+export default request
